c4fa5933f93e1ea73ffd1864961c6311e5e3d2f2
mt-cs <mtania@dons.usfca.edu>
2021-04-04 18:05:49 -0700

    IO write and append

--- shell.c
+++ shell.c
@@ -152,6 +152,13 @@ int main(void)
             char **args = elist_get(list, 0); // we get the first element, we will dump args to execvp
             LOG("args is: %s\n", *args);
             LOG("args[0] is: %s\n", args[0]);
+
+            bool write = false;
+            bool read = false;
+            bool append = false;
+            // bool red_stdin = true;
+            // bool redirect = true;
+            
             // Use dup2 to achieve this; 
             // right before the newly-created child process calls execvp, 
             // you will open the appropriate files and set up redirection with dup2.
@@ -162,119 +169,78 @@ int main(void)
             //ssize_t index = elist_index_of(list, ">");
             ssize_t index = -1;
 
+            //loop through the args
             for (int i = 0; i < elist_size(list) - 1; ++i) {
-
+                LOG("args[i] = %s\n", args[i]);
                 if (strcmp(args[i], ">") == 0) {
                     index = i;
-                    int open_flags = O_RDWR | O_CREAT | O_TRUNC;
-
-                    int open_perms = 0666;
-
+                    write = true;
+                    // LOG("Index > is: %ld\n", index);
+                    // LOG("Args > at index is: %s\n", args[index]);
+                    break;
+                } else if (strcmp(args[i], "<") == 0) {
+                    index = i;
+                    read = true;
+                    // LOG("Index < is: %ld\n", index);
+                    // LOG("Args < at index is: %s\n", args[index]);
+                    break;
+                } else if (strcmp(args[i], ">>") == 0) {
+                    index = i;
+                    append = true;
+                    // LOG("Index >> is: %ld\n", index);
+                    // LOG("Args >> at index is: %s\n", args[index]);
+                    break;
+                } 
+            }
+            LOG("Index now is: %ld\n", index);
+          
+            if(index != -1) { // means > < >> is in token
+                args[index] = 0;
+                int open_perms = 0666;
+                if (append) {
+                    int open_flags = O_RDWR | O_CREAT | O_APPEND;
+                    LOGP ("appending....\n");
                     int fd = open(args[index + 1], open_flags, open_perms);
+                    LOG("Filename: %s\n", args[index + 1]);
                     if (fd == -1) {
                         perror("open");
                         return 1;
                     }
                     dup2(fd, fileno(stdout));
-                    if(dup2(fd, fileno(stdout)) == -1) {
+                    if (dup2(fd, fileno(stdout)) == -1) { 
                         perror("dup2");
                         return 1;
+                    }    
+
+                } else { 
+                    int open_flags = O_RDWR | O_CREAT | O_TRUNC;
+                    /**
+                    * These are the file permissions we see when doing an `ls -l`: we can
+                    * restrict access to the file. 0666 is octal notation for allowing all file
+                    * permissions, which is then modified by the user's 'umask.'
+                    */
+                    int fd = open(args[index + 1], open_flags, open_perms);
+                    LOG("Filename: %s\n", args[index + 1]);
+                    if (fd == -1) {
+                        perror("open");
+                        return 1;
                     }
-
-                } else if (strcmp(args[i], ">>") == 0){
-                    index = i;
-
-                    int open_flags = O_RDWR | O_CREAT | O_APPEND;   //---> changed to O_APPEND
-                        int open_perms = 0666;
-                    
-                        int fd = open(args[index + 1], open_flags, open_perms);
-                        if (fd == -1) {
-                            perror("open");
-                            return 1;
-                        }
-
-                        if (dup2(fd, fileno(stdout)) == -1) {
+                    if (write) {
+                        dup2(fd, fileno(stdout));
+                        if (dup2(fd, fileno(stdout)) == -1) { 
                             perror("dup2");
                             return 1;
-                    }
-
-                } else if (strcmp(args[i], "<") == 0){
-                    index = i;
-
-                    int open_flags = O_RDWR | O_CREAT | O_TRUNC;  
-                        int open_perms = 0666;
-                    
-                        int fd = open(args[index + 1], open_flags, open_perms);
-                        if (fd == -1) {
-                            perror("open");
-                            return 1;
-                        }
-
-                        if (dup2(fd, fileno(stdin)) == -1) {
+                        }    
+                    } else if (read) {
+                        dup2(fd, fileno(stdin));
+                        if (dup2(fd, fileno(stdin)) == -1) { 
                             perror("dup2");
                             return 1;
                         }
-                }
-            
-            }
-
-            //loop through the args
-            // for (int i = 0; i < elist_size(list) - 1; ++i) {
-            //     LOG("args[i] = %s\n", args[i]);
-            //     if (strcmp(args[i], ">") == 0) {
-            //         index = i;
-            //         LOG("Index > is: %ld\n", index);
-            //         LOG("Args > at index is: %s\n", args[index]);
-            //         break;
-            //     } else if (strcmp(args[i], "<") == 0) {
-            //         index = i;
-            //         LOG("Index < is: %ld\n", index);
-            //         LOG("Args < at index is: %s\n", args[index]);
-            //         break;
-            //     } else {
-            //         LOGP("We are in the else clause\n");
-            //     }
-            // }
-
-            // LOG("Index now is: %ld\n", index);
-            // char **redirect = elist_get(list, index);
-            // LOG("redirect inside if is %s\n", *redirect);
-            // if(index != -1) { // means > is in token
-            //     args[index] = 0;
-            //     int open_flags = O_RDWR | O_CREAT | O_TRUNC;
-            //     /**
-            //     * These are the file permissions we see when doing an `ls -l`: we can
-            //     * restrict access to the file. 0666 is octal notation for allowing all file
-            //     * permissions, which is then modified by the user's 'umask.'
-            //     */
-            //     int open_perms = 0666;
-
-            //     int fd = open(args[index + 1], open_flags, open_perms);
-            //     if (fd == -1) {
-            //         perror("open");
-            //         return 1;
-            //     }
+                    }       
+                } 
+            }    
                 
-            //     LOG("redirect inside if is %s\n", *redirect);
-            //     if ((strcmp(*redirect, ">") == 0)) {
-            //         dup2(fd, fileno(stdout));
-            //         if (dup2(fd, fileno(stdout)) == -1) { 
-            //             perror("dup2");
-            //             return 1;
-            //         }
-            //     } else if ((strcmp(*redirect, "<") == 0)) {
-            //         dup2(fd, fileno(stdin));
-            //         if (dup2(fd, fileno(stdin)) == -1) { 
-            //             perror("dup2");
-            //             return 1;
-            //         }
-            //     }       
-            // } else{
-            //     LOGP("No > or < token");
-            // }
-
-            
-
             if(execvp(args[0], args) == -1) { // dump args to execvp
                 perror("execvp"); // this will only execute if execvp fail
                 close(fileno(stdin));
@@ -282,7 +248,6 @@ int main(void)
                 close(fileno(stderr));
                 exit(1);
             }
-            exit(0); // why exit(0) shell 194?
         } else {
             /* I am the parent */
             int status; //synchronize the processes
@@ -299,7 +264,7 @@ int main(void)
     return 0;
 }
 
-/*call dup2, sigchld*/
+/* call dup2, sigchld*/
 /* our shell is an unkillable process 
  * ignore sigint */
 
